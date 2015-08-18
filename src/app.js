164217992cc7e747e0492e2af2361fc4b8af30db
willowcheng <e.wingus@Gmail.com>
2015-08-18 00:22:43 -0400

    Shaky3D action

--- src/app.js
+++ src/app.js
@@ -11,12 +11,12 @@ var HelloWorldLayer = cc.Layer.extend({
         // ask the window size
         var size = cc.winSize;
 
-        var role = new cc.Sprite(res.role_jpg);
-        this.addChild(role);
-        role.setPosition(size.width / 2, size.height / 2);
-        role.setScale(0.5);
+        /*var role = new cc.Sprite(res.role_jpg);
+         this.addChild(role);
+         role.setPosition(size.width / 2, size.height / 2);
+         role.setScale(0.5);
 
-        role.setPositionX(100);
+         role.setPositionX(100);*/
         //role.runAction(new cc.MoveTo(2, cc.p(size.width - 100, size.height / 2)));
         //role.runAction(new cc.moveBy(2, cc.p(500, 0)));
 
@@ -28,9 +28,20 @@ var HelloWorldLayer = cc.Layer.extend({
          var move2 = move1.reverse();
          role.runAction(new cc.Sequence(move1, move2));*/
 
-        var move = new cc.MoveBy(2, cc.p(500, 0));
-        var scale = new cc.ScaleTo(2, 1);
-        role.runAction(new cc.Spawn(move, scale));
+        //var move = new cc.MoveBy(2, cc.p(500, 0));
+        //var scale = new cc.ScaleTo(2, 1);
+        //role.runAction(new cc.Spawn(move, scale));
+
+        /*role.runAction(new cc.Sequence(new cc.Spawn(move, scale), new cc.CallFunc(function () {
+         cc.log("PLAY COMPLETE");
+         })));*/
+
+        var role = new cc.NodeGrid();
+        role.addChild(new cc.Sprite(res.role_jpg));
+        role.setPosition(size.width / 2, size.height / 2);
+        this.addChild(role);
+
+        role.runAction(new cc.Shaky3D(10, cc.size(50, 50), 5, false));
 
         return true;
     }
